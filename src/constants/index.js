3631f7ebcd10072b988ba33114053c67a36875c7
Rastin Mehr <rastin@timble.net>
2018-03-17 17:40:00 -0700

    person constants not needed for now

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,12 +2,10 @@ import Auth from './auth';
 import Storage from './storage';
 import Actors from './actors';
 import Actor from './actor';
-import Person from './person';
 
 export {
   Auth,
   Storage,
   Actors,
   Actor,
-  Person,
 };
--- src/constants/person.js
+++
@@ -1,9 +0,0 @@
-import async from './async';
-
-const person = async('person');
-export default {
-  READ: person('read'),
-  EDIT: person('edit'),
-  ADD: person('add'),
-  DELETE: person('delete'),
-};
