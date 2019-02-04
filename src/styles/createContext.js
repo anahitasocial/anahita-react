353091a741e4667280ab45a4e36c44781de95257
Rastin Mehr <rastin@timble.net>
2019-02-03 23:53:35 -0800

    using jss from react-jss

--- src/styles/createContext.js
+++ src/styles/createContext.js
@@ -1,9 +1,7 @@
-import { create } from 'jss'
-import preset from 'jss-preset-default'
-import { SheetsRegistry } from 'react-jss/lib/jss'
-import { createMuiTheme } from '@material-ui/core/styles'
-import createGenerateClassName from '@material-ui/core/styles/createGenerateClassName'
-import { red } from '@material-ui/core/colors'
+import jss, { SheetsRegistry } from 'react-jss/lib/jss';
+import { createMuiTheme } from '@material-ui/core/styles';
+import createGenerateClassName from '@material-ui/core/styles/createGenerateClassName';
+import { red } from '@material-ui/core/colors';
 
 const theme = createMuiTheme({
   typography: {
@@ -14,13 +12,14 @@ const theme = createMuiTheme({
       main: '#098ED1',
     },
     error: red,
-  }
-})
+  },
+});
 
-const jss = create(preset());
+// const jss = create(preset());
 jss.options.createGenerateClassName = createGenerateClassName;
 
-export const sheetsManager: Map<*, *> = new Map();
+// export const sheetsManager: Map<*, *> = new Map();
+export const sheetsManager = new Map();
 
 export default function createContext() {
   return {
@@ -29,6 +28,6 @@ export default function createContext() {
     // This is needed in order to deduplicate the injection of CSS in the page.
     sheetsManager,
     // This is needed in order to inject the critical CSS.
-    sheetsRegistry: new SheetsRegistry()
-  }
+    sheetsRegistry: new SheetsRegistry(),
+  };
 }
