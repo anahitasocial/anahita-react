ef15e3f0ce64f96bd711bce9a4a7f560966a056b
Rastin Mehr <rastin@timble.net>
2019-07-23 11:08:44 -0700

    #4 - ho data flow - fixed: like action isLiked checking

--- src/containers/stories/Browse.js
+++ src/containers/stories/Browse.js
@@ -181,7 +181,7 @@ class StoriesBrowse extends React.Component {
                     story.object && isLikeable(story.object) &&
                     <LikeAction
                       node={story.object}
-                      isLiked={story.isVotedUp}
+                      isLiked={story.commands.includes('unvote')}
                       key={`story-like-${story.id}`}
                     />,
                     story.object && isCommentable(story.object) &&
