162e90367047a36ae633ab7e9fb925c733e99e91
Rastin Mehr <rastin@timble.net>
2020-04-23 23:28:50 -0700

    fixed Person proptype file case

---
+++ src/proptypes/Person.js
@@ -0,0 +1,48 @@
+import {
+  shape,
+  number,
+  string,
+  oneOf,
+  objectOf,
+  any,
+} from 'prop-types';
+
+import PERSON from '../constants/person';
+import ImageURL from './ImageURL';
+
+const { TYPE, GENDER } = PERSON.FIELDS;
+
+export default shape({
+  id: number,
+  objectType: oneOf(['com.people.person']),
+  name: string,
+  alias: string,
+  username: string,
+  email: string,
+  password: string,
+  type: oneOf([
+    TYPE.GUEST,
+    TYPE.REGISTERED,
+    TYPE.ADMIN,
+    TYPE.SUPER_ADMIN,
+  ]),
+  givenName: string,
+  familyName: string,
+  body: string,
+  gender: oneOf([
+    GENDER.NEUTRAL,
+    GENDER.FEMALE,
+    GENDER.MALE,
+    GENDER.OTHER,
+    '',
+  ]),
+  gadgets: objectOf(any),
+  imageURL: ImageURL,
+  coverURL: ImageURL,
+  followerCount: number,
+  leaderCount: number,
+  mutualCount: number,
+  subscriberCount: number,
+  creationTime: string,
+  updateTime: string,
+});
