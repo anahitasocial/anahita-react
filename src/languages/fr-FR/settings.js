70d001e03ebaa71e14495caf0d3ac742e01b7387
rmdstudio <rastin@rmdstudio.com>
2021-08-15 23:16:27 -0700

    updated translation

--- src/languages/fr-FR/settings.js
+++ src/languages/fr-FR/settings.js
@@ -1,11 +1,51 @@
 export default {
   app: {
+    blogs: {
+      ownerIds: 'Owner Ids',
+      createdByIds: 'Created by Ids',
+    },
+    documents: {
+      uploadlimit: 'Upload Limit',
+      uploadlimitOptions: {
+        '2Mb': '2 MB',
+        '4Mb': '4 MB',
+        '8Mb': '8 MB',
+      },
+    },
     groups: {
       canPublish: 'Can publish',
       canPublishOptions: {
         siteAdmins: 'Site Admins',
         siteAdminsRegisteredMembers: 'Site Admins, Registered Members',
       },
+    },
+    locations: {
+      service: 'Service',
+      apiKeyGeocoding: 'Geocoding API Key',
+      apiKeyMaps: 'Maps API Key',
+    },
+    notifications: {
+      useCron: 'Use Cron',
+      useCronOptions: {
+        no: 'No',
+        yes: 'Yes',
+      },
+      muteEmail: 'Mute Email Notifications',
+      muteEmailOptions: {
+        no: 'No',
+        yes: 'Yes',
+      },
+    },
+    mailer: {
+      debug: 'Debug Mailer',
+      redirectEmail: 'Redirect Email',
+      debugOptions: {
+        no: 'No',
+        yes: 'Yes',
+      },
+    },
+    pages: {
+      contentPath: 'Content path',
     },
     people: {
       allowRegistration: 'Allow guest users to sign up',
@@ -19,9 +59,64 @@ export default {
         libAnPrivacylabelRegistered: 'Registered',
       },
     },
+    photos: {
+      uploadlimit: 'Upload Limit',
+      uploadlimitOptions: {
+        '2Mb': '2 MB',
+        '4Mb': '4 MB',
+        '8Mb': '8 MB',
+        '10Mb': '10 MB',
+      },
+    },
     subscriptions: {
       login: 'Paypal Login',
       password: 'Paypal Password',
+    },
+  },
+  assignment: {
+    options: {
+      always: 'Always',
+      optional: 'Optional',
+      never: 'Never',
+    },
+  },
+  plugin: {
+    local: {
+      storage: {
+        folder: 'Local storage folder',
+      },
+    },
+    s3: {
+      storage: {
+        folder: 'S3 storage folder',
+        region: 'Region',
+        regionOptions: {
+          asiaPacificHongKong: 'Asia Pacific, Hong Kong',
+          asiaPacificMumbai: 'Asia Pacific, Mumbai',
+          asiaPacificOsakaLocal: 'Asia Pacific, Osaka Local',
+          asiaPacificSeoul: 'Asia Pacific, Seoul',
+          asiaPacificSingapore: 'Asia Pacific, Singapore',
+          asiaPacificSydney: 'Asia Pacific, Sydney',
+          asiaPacificTokyo: 'Asia Pacific, Tokyo',
+          canadaCentral: 'Canada, Central',
+          chinaBeijing: 'China, Beijing',
+          chinaNingxia: 'China, Ningxia',
+          euFrankfurt: 'EU, Frankfurt',
+          euIreland: 'EU, Ireland',
+          euLondon: 'EU, London',
+          euParis: 'EU, Paris',
+          euStockholm: 'EU, Stockholm',
+          middleEastBahrain: 'Middle East, Bahrain',
+          southAmericaSaoPaulo: 'South America, Sao Paulo',
+          usEastNVirginiaAwsDefault: 'US East, N. Virginia (AWS Default)',
+          usEastOhio: 'US East, Ohio',
+          usWestNCalifornia: 'US West, N. California',
+          usWestOregon: 'US West, Oregon',
+        },
+        bucket: 'Bucket',
+        accessKey: 'Access Key',
+        secretKey: 'Secret Key',
+      },
     },
   },
 };
