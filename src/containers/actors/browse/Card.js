09ba0bd7c7af450d4bc4a386a45f1ae1f642a568
rmdstudio <rastin@rmdstudio.com>
2022-08-21 22:30:34 -0700

    Person Delete for Admins - added: utils.node.isSuperAdmin method - added: delete actor to actor card for admins

--- src/containers/actors/browse/Card.js
+++ src/containers/actors/browse/Card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import DeleteAction from '../../actions/Delete';
 import FollowAction from '../../actions/Follow';
 import ActorCard from '../../../components/cards/Actor';
 import permissions from '../../../permissions/actor';
@@ -12,17 +13,18 @@ const ActorsCard = (props) => {
     actor,
     viewer,
   } = props;
+
   const canFollow = permissions.canFollow(actor);
+  const canAdminister = permissions.canAdminister(viewer) && viewer.id !== actor.id;
 
   return (
     <ActorCard
       actor={actor}
       viewer={viewer}
-      action={canFollow &&
-        <FollowAction
-          actor={actor}
-        />
-      }
+      action={[
+        canFollow && <FollowAction actor={actor} />,
+        canAdminister && <DeleteAction node={actor} />,
+      ]}
     />
   );
 };
