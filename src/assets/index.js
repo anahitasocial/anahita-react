7fcd33776728d9b5e2225077a709a039444bf1f3
rmdstudio <rastin@rmdstudio.com>
2022-09-14 23:01:01 -0700

    hot fix

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,7 @@
 /* eslint-disable global-require */
 if (process.env.REACT_APP_ASSETS) {
-  const assets = `./${process.env.REACT_APP_ASSETS}`;
   // eslint-disable-next-line import/no-dynamic-require
-  module.exports = require(assets);
+  module.exports = require(`./${process.env.REACT_APP_ASSETS}`);
 } else {
   module.exports = require('./default');
 }
