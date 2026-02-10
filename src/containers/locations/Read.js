100cd5624e067c7656cd408ff0535e44575aed78
rmdstudio <rastin@rmdstudio.com>
2026-02-09 23:14:09 -0800

    cleanup

--- src/containers/locations/Read.js
+++ src/containers/locations/Read.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 
 import Avatar from '@material-ui/core/Avatar';
